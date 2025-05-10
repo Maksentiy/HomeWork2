10f819329d0b85112f63765372a1fc403abb1eae
Maksentiy <maksim.konon.96@gmail.com>
2025-05-10 15:07:36 +0300

    Added solution for second home work

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше Имя");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Введите ваш возраст");
+        var age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите ваш рост");
+        var height = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Привет, {name} \n\tВаш возраст: {age} \n\t\tВаш рост: {height} м.");
+    }
+}
